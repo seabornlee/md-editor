6878732c9c075314548a9d7fb60c4f660bba21ec
Seaborn Lee <seabornlee.cn@gmail.com>
2020-02-23 22:22:07 +0800

    refactor: revise test with data-driven test

--- src/utils/markdown-compiler.test.ts
+++ src/utils/markdown-compiler.test.ts
@@ -1,19 +1,11 @@
 import compile from './markdown-compiler'
 
-test('should compile bold text', () => {
-  const md = '**hello** **world**'
-  const html = compile(md)
-  expect(html).toEqual('<b>hello</b> <b>world</b>')
-})
-
-test('should compile H1', () => {
-  const md = '#hello\r\n#world'
-  const html = compile(md)
-  expect(html).toEqual('<h1>hello</h1>\r\n<h1>world</h1>')
-})
-
-test('should compile H2', () => {
-  const md = '##hello\r\n##world'
-  const html = compile(md)
-  expect(html).toEqual('<h2>hello</h2>\r\n<h2>world</h2>')
+describe.each([
+  ['**', '**hello** **world**', '<b>hello</b> <b>world</b>'],
+  ['#', '#hello\r\n#world', '<h1>hello</h1>\r\n<h1>world</h1>'],
+  ['##', '##hello\r\n##world', '<h2>hello</h2>\r\n<h2>world</h2>']
+])('', (tag, markdownText, html) => {
+  test(`should compile ${tag}`, () => {
+    expect(compile(markdownText)).toEqual(html)
+  })
 })
