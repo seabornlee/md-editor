f1a0cb5d204412b6daea3857e7335a8c5636b54f
Along <993909154@qq.com>
2020-02-23 22:47:42 +0800

    feat:h3

--- src/utils/markdown-compiler.test.ts
+++ src/utils/markdown-compiler.test.ts
@@ -3,7 +3,8 @@ import compile from './markdown-compiler'
 describe.each([
   ['**', '**hello** **world**', '<b>hello</b> <b>world</b>'],
   ['#', '#hello\r\n#world', '<h1>hello</h1>\r\n<h1>world</h1>'],
-  ['##', '##hello\r\n##world', '<h2>hello</h2>\r\n<h2>world</h2>']
+  ['##', '##hello\r\n##world', '<h2>hello</h2>\r\n<h2>world</h2>'],
+  ['###', '###hello\r\n###world', '<h3>hello</h3>\r\n<h3>world</h3>']
 ])('', (tag, markdownText, html) => {
   test(`should compile ${tag}`, () => {
     expect(compile(markdownText)).toEqual(html)
