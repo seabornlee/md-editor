33da33482260a4be2dcda45c29f24fb900c8e8da
Seaborn Lee <seabornlee.cn@gmail.com>
2020-02-25 23:41:48 +0800

    test: fix test and remove test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import userEvent from '@testing-library/user-event'
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from './App'
 
 test('it should keep editor content when toggle back from read mode', async () => {
@@ -9,13 +9,8 @@ test('it should keep editor content when toggle back from read mode', async () =
   const editor = getByTestId('editor')
 
   await userEvent.type(editor, 'Hello')
+  userEvent.click(getByTestId('read'))
   userEvent.click(getByTestId('edit'))
 
   expect(editor).toHaveTextContent('Hello')
-})
-
-test('it should listen onMouseUp event and upload image', () => {
-  const { getByTestId } = render(<App />)
-  const editor = getByTestId('editor')
-  fireEvent.mouseUp(editor)
 })
